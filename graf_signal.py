fbc610f7dd8a4956e638392b25453fae539419ce
vmilic <milic.velimir@gmail.com>
2014-05-07 11:53:22 +0200

    Dodao klasu za minutni graf
    Sredio sam izbor tocaka i raspona podataka.
    - problemi s formatiranjem teksta (resize cropa labele)
    - span selector radi s lijevim klikom (click and drag), ali samo ljevi
    klik na bilo koje mjesto na grafu takodjer radi. emita isti xmin i xmax.
    To ne bi trebao biti problem po meni.

--- graf_signal.py
+++ graf_signal.py
@@ -11,6 +11,7 @@ Izdvajanje grafova u pojedine klase. Dodana je mini aplikacija za funkcionalno t
 import sys
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+from matplotlib.widgets import SpanSelector
 
 from PyQt4 import QtGui,QtCore
 import pandas as pd
@@ -45,10 +46,10 @@ class GrafSatniSrednjaci(MPLCanvas):
     subklasa MPLCanvas, ona definira detalje crtanja isl.
     SIGNALI:
     odabirSatni(PyQt_PyObject)
-        -odabir jednog satnog agregata
+        -odabir jednog satnog agregata, lijevi klik
         -emitira listu. [vrijeme(timestamp)]
     flagSatni(PyQt_PyObject)
-        -promjena flaga jednog satnog agregata
+        -promjena flaga jednog satnog agregata, desni klik
         -emitira listu. [vrijeme(timestamp), novi flag]
     """      
     def __init__(self,*args,**kwargs):
@@ -144,20 +145,118 @@ class GrafMinutniPodaci(MPLCanvas):
     Graf za crtanje minutnih podataka.
     Ulaz:
     data je lista pandas datafrejmova, satni slice koncetracije
-       0 cijeli -> cijeli dataframe (sluzi za plot linije)
-       1 pozitivanFlag ->dio dataframe gdje je flag >=0 (za scatter plot)
-       2 negativanFlag ->dio dataframe gdje je flag <0 (za scatter plot)
+       [0] cijeli -> cijeli dataframe (sluzi za plot linije)
+       [1] pozitivanFlag ->dio dataframe gdje je flag >=0 (za scatter plot)
+       [2] negativanFlag ->dio dataframe gdje je flag <0 (za scatter plot)
+       
+    SIGNALI:
+    flagTockaMinutni(PyQt_PyObject)
+        -izbor jedne tocke kojoj treba promjeniti flag - desni klik
+        -emitira listu - [vrijeme(timestamp),novi flag]
+    flagSpanMinutni(PyQt_PyObject)
+        -izbor spana, lijevi klik & drag. na klik granice su identicne
+        -emitira listu - [vrijeme min, vrijeme max, novi flag]
+        -min i max granice su isti broj na jedan ljevi klik...problem?
+        
+    arg=[minOznaka,maxOznaka,flag]
+            self.emit(QtCore.SIGNAL('flagSpanMinutni(PyQt_PyObject)'),arg)
     """
     def __init__(self,*args,**kwargs):
         MPLCanvas.__init__(self,*args,**kwargs)
+        self.donjaGranica=None
+        self.gornjaGranica=None
+        self.veze()
+    
+    def veze(self):
+        #desni klik za izbor jedne točke
+        self.cidpick=self.mpl_connect('pick_event',self.onpick)
+        #span selector
+        self.span=SpanSelector(self.axes,
+                               self.minutni_span_flag,
+                               'horizontal',
+                               useblit=True,
+                               rectprops=dict(alpha=0.5,facecolor='tomato')
+                               )
+    
+    def minutni_span_flag(self,xmin,xmax):
+        xmin=round(xmin)
+        xmax=round(xmax)
+        puniSatMin=False
+        puniSatMax=False
         
+        if xmin==60:
+            puniSatMin=True
+            xmin='00'
+        else:
+            xmin=str(xmin)
+            
+        if xmax==60:
+            puniSatMax=True
+            xmax='00'
+        else:
+            xmax=str(xmax)
+            
+        #sastavi timestamp
+        godina=str(self.donjaGranica.year)
+        mjesec=str(self.donjaGranica.month)
+        dan=str(self.donjaGranica.day)
+        sat=str(self.donjaGranica.hour)
+        timeMin=godina+'-'+mjesec+'-'+dan+' '+sat+':'+xmin+':'+'00'
+        timeMax=godina+'-'+mjesec+'-'+dan+' '+sat+':'+xmax+':'+'00'
+        minOznaka=pd.to_datetime(timeMin)
+        maxOznaka=pd.to_datetime(timeMax)
+        
+        if puniSatMin:
+            minOznaka=minOznaka+timedelta(hours=1)
+            
+        if puniSatMax:
+            maxOznaka=maxOznaka+timedelta(hours=1)
+            
+        opis='Odabrani interval od '+str(minOznaka)+' do '+str(maxOznaka)
+        tekst='Odaberi novu vrijednost flaga:'
+        flag,ok=QtGui.QInputDialog.getDouble(self,opis,tekst)
+        if ok:
+            arg=[minOznaka,maxOznaka,flag]
+            self.emit(QtCore.SIGNAL('flagSpanMinutni(PyQt_PyObject)'),arg)
+        
+    def onpick(self,event):
+        puniSat=False
+        izbor=round(event.mouseevent.xdata)
+        if izbor==60:
+            puniSat=True
+            izbor='00'
+        else:
+            izbor=str(izbor)
+        #sastavi timestamp
+        godina=str(self.donjaGranica.year)
+        mjesec=str(self.donjaGranica.month)
+        dan=str(self.donjaGranica.day)
+        sat=str(self.donjaGranica.hour)
+        time=godina+'-'+mjesec+'-'+dan+' '+sat+':'+izbor+':'+'00'
+        timeOznaka=pd.to_datetime(time)
+        if puniSat:
+            timeOznaka=timeOznaka+timedelta(hours=1)
+        
+        #desni klik -  promjena flaga jedne tocke
+        if event.mouseevent.button==3:
+            opis='Odabrano vrijeme: '+str(timeOznaka)
+            tekst='Odaberi novu vrijednost flaga:'
+            flag,ok=QtGui.QInputDialog.getDouble(self,opis,tekst)
+            if ok:
+                arg=[timeOznaka,flag]
+                self.emit(QtCore.SIGNAL('flagTockaMinutni(PyQt_PyObject)'),arg)
+
     def crtaj(self,data):
         """
         x os plota će biti minute timestampa od 1 do 60
         """
-
+        #zapamti podatke o timestampu za datum i sat
+        self.donjaGranica=data[0].index.min()
+        self.gornjaGranica=data[0].index.max()
         #priprema za crtanje
         self.axes.clear()
+        title='Minutni podaci od: '+str(self.donjaGranica)+' do: '+str(self.gornjaGranica)
+        self.axes.set_title(title,fontsize=4)
         linija=data[0]
         tockeOk=data[1]
         tockeNo=data[2]
@@ -185,19 +284,29 @@ class GrafMinutniPodaci(MPLCanvas):
         
         if len(xMinute)!=0:
             self.axes.plot(xMinute,linija.values,
-                           color='black',
-                           alpha=0.8)
+                           color='black')
                        
         if len(xFlagOk)!=0:
             self.axes.scatter(xFlagOk,tockeOk.values,
                               color='green',
-                              marker='o')
+                              marker='o',
+                              picker=2,
+                              alpha=0.4)
         
         if len(xFlagNo)!=0:
             self.axes.scatter(xFlagNo,tockeNo.values,
                               color='red',
-                              marker='o')
-                              
+                              marker='o',
+                              picker=2,
+                              alpha=0.4)
+        
+        xLabels=self.axes.get_xticklabels()
+        for label in xLabels:
+            label.set_fontsize(4)
+            
+        yLabels=self.axes.get_yticklabels()
+        for label in yLabels:
+            label.set_fontsize(4)                      
         #pokusaj automatskog proavnavanja
         self.axes.set_xlim(1,60)
         self.fig.tight_layout()
@@ -225,6 +334,8 @@ class ApplicationMain(QtGui.QMainWindow):
         #connect to test signal
         self.connect(canvasSatni,QtCore.SIGNAL('flagSatni(PyQt_PyObject)'),self.test_print)
         self.connect(canvasSatni,QtCore.SIGNAL('odabirSatni(PyQt_PyObject)'),self.test_print)
+        self.connect(canvasMinutni,QtCore.SIGNAL('flagTockaMinutni(PyQt_PyObject)'),self.test_print)
+        self.connect(canvasMinutni,QtCore.SIGNAL('flagSpanMinutni(PyQt_PyObject)'),self.test_print)
         
         """
         Testni podaci, dictionary pandas datafrejmova koji je rezultat agregatora.

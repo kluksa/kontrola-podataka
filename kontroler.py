a9df744cf969612f23cf720d696875815f9b571e
vmilic <milic.velimir@gmail.com>
2014-05-12 11:56:41 +0200

    Problem s connectionima
    -problem s prosljeđivanjem argumenta signala metodama

--- kontroler.py
+++ kontroler.py
@@ -3,6 +3,9 @@
 Created on Fri May  9 13:19:45 2014
 
 @author: velimir
+
+Problemi:
+connect1 prima dobar signal, slot radi problem. citaj() missing 1 requred positional argument :'path'
 """
 
 from PyQt4 import QtGui,QtCore
@@ -10,15 +13,20 @@ from PyQt4 import QtGui,QtCore
 class KontrolerSignala(QtGui.QWidget):
     """
     Kontrolni dio aplikacije. Subklasa je QWidgeta zbog metode connect
+    view je objekt displaya, model je objekt dokumenta
     """
     def __init__(self,parent=None,view=None,model=None):
         QtGui.QWidget.__init__(self,parent)
         
-        #connect:qt akcija ucitavanja - read csv u dokumentu
+        #connect1:qt akcija ucitavanja - read csv u dokumentu
         self.connect(view,
                     QtCore.SIGNAL('request_read_csv(PyQt_PyObject)'),
                     model.citaj_csv)
-        #connect: trenutni kljucevi - update QComboBox liste u view
+        #connect2: trenutni kljucevi - update QComboBox liste u view
         self.connect(model,
                     QtCore.SIGNAL('update_kljuc(PyQt_PyObject)'),
                     view.update_kljuc)
+                    
+    def test(self,x):
+        print(x)
+        

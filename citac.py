1f33a455762ea38b63009fa4eb0334f8d1715993
vmilic <milic.velimir@gmail.com>
2014-07-10 12:16:22 +0200

    velika izmjena citaca
    Citac radi bez definiranih kanala.
    -Ide stupac po stupac, ako u nazivu stupca nema 'status' i 'flag' stvara
    novi frame, a ako ima te stupce pridodaje ih prethodnom frejmu
    
    P.S. prilikom save u csv, nailazim na mali problem. Do sada nisam uspio
    pronaci zgodan nacin da rastavim index u 2 stupca (date i time) pa sam
    implementirao brute force rijesenje (koje je kriminalno sporo u odnosu
    na ostatak koda - slobodna procjena... 90% vremena otpada na to)

--- citac.py
+++ citac.py
@@ -8,145 +8,200 @@ Created on Mon Apr 14 08:01:06 2014
 import pandas as pd
 import re
 
+def test_frames(frames1,frames2):
+    ukupno = True
+    if len(frames1.keys()) == len(frames2.keys()):
+        print('OK!, isti broj frejmova')
+        ukupno = ukupno and True
+    else:
+        print('ERROR!, broj frejmova ne odgovara')
+        ukupno = ukupno and False
+        
+    f1=list(frames1.keys())
+    f2=list(frames2.keys())
+    for frame in f1:
+        if frame in f2:
+            print('OK!, {0} frame je u obje liste'.format(frame))
+            ukupno = ukupno and True
+        else:
+            print('ERROR!, {0} nije u drugoj listi'.format(frame))
+            ukupno = ukupno and False
+    if ukupno:
+        print('Test OK!')
+    else:
+        print('Error!')
+
 class WlReader:
 
-    def __init__(self):
-        self.kanali = {'SO2':'1-SO2-ppb',
-                       'CO':'30-CO-ppm',}
-        """
-        key:regex pattern to match it
-        
-        pretpostavka je da je naziv kompaktan i da sadrži:
-        ime plina i mjernu jedinicu
-
-	pattern je 
-	[0-9]+-xxx-yyy
-
-        """
-        self.regexKanali={'SO2-ug/m3':r'\b\S*so2\S*ug/m3\b',
-                          'NO-ug/m3':r'\b\S*no\S*ug/m3\b',
-                          'NOx-ug/m3':r'\b\S*nox\S*ug/m3\b',
-                          'NO2-ug/m3':r'\b\S*no2\S*ug/m3\b',
-                          'CO-mg/m3':r'\b\S*co\S*mg/m3\b',
-                          'O3-ug/m3':r'\b\S*o3\S*ug/m3\b',
-                          'PM1-ug/m3':r'\b\S*pm1\S*ug/m3\b',
-                          'PM2.5-ug/m3':r'\b\S*(pm2\.5)\S*ug/m3\b',
-                          'PM10-ug/m3':r'\b\S*pm10\S*ug/m3\b'}
-
-    def set_kanali_za_citanje(self, mapa):
-        self.kanali = mapa
-        
-    def dodaj_kanal(self,kljuc,kanal):
-        if kljuc not in self.kanali.keys():
-            self.kanali[kljuc]=kanal
-        else:
-            raise KeyError('Unable to create new key, it already exists')
-   
-    def brisi_kanal(self,dkey):
-        del self.kanali[dkey]
+#    def __init__(self):
+#        self.kanali = {'SO2':'1-SO2-ppb',
+#                       'CO':'30-CO-ppm',}
+#        """
+#        key:regex pattern to match it
+#        
+#        pretpostavka je da je naziv kompaktan i da sadrži:
+#        ime plina i mjernu jedinicu
+#
+#	pattern je 
+#	[0-9]+-xxx-yyy
+#
+#        """
+#        self.regexKanali={'SO2-ug/m3':r'\b\S*so2\S*ug/m3\b',
+#                          'NO-ug/m3':r'\b\S*no\S*ug/m3\b',
+#                          'NOx-ug/m3':r'\b\S*nox\S*ug/m3\b',
+#                          'NO2-ug/m3':r'\b\S*no2\S*ug/m3\b',
+#                          'CO-mg/m3':r'\b\S*co\S*mg/m3\b',
+#                          'O3-ug/m3':r'\b\S*o3\S*ug/m3\b',
+#                          'PM1-ug/m3':r'\b\S*pm1\S*ug/m3\b',
+#                          'PM2.5-ug/m3':r'\b\S*(pm2\.5)\S*ug/m3\b',
+#                          'PM10-ug/m3':r'\b\S*pm10\S*ug/m3\b'}
+#
+#    def set_kanali_za_citanje(self, mapa):
+#        self.kanali = mapa
+#        
+#    def dodaj_kanal(self,kljuc,kanal):
+#        if kljuc not in self.kanali.keys():
+#            self.kanali[kljuc]=kanal
+#        else:
+#            raise KeyError('Unable to create new key, it already exists')
+#   
+#    def brisi_kanal(self,dkey):
+#        del self.kanali[dkey]
 
     def citaj(self, path):
-        """reads from CSV file into data frame (path is location of file)"""
+        """
+        Cita iz CSV filea u data frame objekte
+        struktura csv, (sve u uglatim zagradama je opcionalno):
+        Name,[status],[flag], ... , NameX,[statusX],[flagx],Flag,Zone
+        """
         df = pd.read_csv(
             path,
-            na_values='-999.00',
-            index_col=0,
-            parse_dates=[[0,1]],
-            dayfirst=True,
-            header=0,
-            sep=',',
-            encoding='latin-1')
-
-
-        if self.kanali=={}:
-            raise KeyError('Neko sranje sa kanalima')
+            na_values = '-999.00',
+            index_col = 0,
+            parse_dates = [[0,1]],
+            dayfirst = True,
+            header = 0,
+            sep = ',',
+            encoding = 'latin-1')
+        
+        dfShort = df.iloc[:,:-2]
+        frejmovi = {}
+        indeks = dfShort.index
+        defaultSeries = pd.Series(0, index = indeks)
+    
+        for colName in dfShort:
+            match1 = re.search(r'status', colName, re.IGNORECASE)
+            match2 = re.search(r'flag', colName, re.IGNORECASE)
+            #statusna var, sadrzi prethodno upisani kljuc
+            pName = None
+            if (not match1) and (not match2):
+                pName = colName
+                frejmovi[pName] = pd.DataFrame({
+                    u'koncentracija':dfShort.loc[:,colName],
+                    u'status':defaultSeries,
+                    u'flag':defaultSeries},
+                    columns =[u'koncentracija',u'status',u'flag'])
             
-        frejmovi={}
-        for k in self.kanali:
-            v=self.kanali[k]
-            i=df.columns.get_loc(v)
-            frejmovi[k] = df.iloc[:,i:i+2]
-            frejmovi[k][u'flag']=pd.Series(0,index=frejmovi[k].index)
-            tmp=frejmovi[k].columns.values
-            tmp[0]=u'koncentracija'
-            tmp[1]=u'status'
-            frejmovi[k].columns=tmp
+            if match1 and (pName != None):
+                frejmovi[pName][u'status'] = dfShort.loc[:,colName]
+            
+            if match2 and (pName != None):
+                frejmovi[pName][u'flag'] = dfShort.loc[:,colName]
         
-        #regex dio
-        for column in df.columns:
-            for key in self.regexKanali:
-                match=re.search(self.regexKanali[key],column,re.IGNORECASE)
-                if match:
-                    v=column
-                    i=df.columns.get_loc(v)
-                    frejmovi[key] = df.iloc[:,i:i+2]
-                    frejmovi[key][u'flag']=pd.Series(0,index=frejmovi[key].index)
-                    tmp=frejmovi[key].columns.values
-                    tmp[0]=u'koncentracija'
-                    tmp[1]=u'status'
-                    frejmovi[key].columns=tmp
-        return frejmovi
-        
+        #dodaj flag i zone na u dictionary (kao pandas dataframe objekte)
+        frejmovi[u'Flag'] = pd.DataFrame({u'Flag':df.loc[:,u'Flag']})
+        frejmovi[u'Zone'] = pd.DataFrame({u'Zone':df.loc[:,u'Zone']})
+    
+        return frejmovi        
         
     #writer metoda
     def save_work(self,frejmovi,filename):
-        """writes to csv file - recimo da radi"""
-        keyList=list(frejmovi.keys())
-        indeks=frejmovi[keyList[0]].index
-        dfAll=pd.DataFrame(index=indeks)
-        dfCurrent=None
+        """
+        funkcija sprema niz frejmova u jedan csv file
+        -flag i Zone se moraju dodati na kraju 
+        
+        konzistencija
+        -u pisanju u csv moram rastaviti date_time u 2 stupca da bih mogao koristiti
+        istu metodu citaj (u protivnom, citaj "pojede" prvi stupac)
+        """
+        keys = list(frejmovi.keys())
+        for key in keys:
+            if (key != u'Flag') and (key != u'Zone'):
+                validKey = key
+    
+        indeks = frejmovi[validKey].index
+    
+        """
+        objasnjenje za skalameriju ispod...
+        citac se lomi prilikom citanja saveanog filea zbog tretiranja prvog stupca, tj.
+        indeksa. Moram razdvojiti datetime index u date i time stupce...
+        """
+        datumFix = pd.DataFrame(
+            {u'Date':pd.Series(0,index = indeks),
+             u'Time':pd.Series(0,index = indeks)},
+            index = indeks)
+        #ova for petlja pojede jako puno vremena u odnosu na ostatak (priblizno 90%)
+        for row in range(len(datumFix.index)):
+            datumFix.iloc[row,0] = datumFix.index[row].strftime('%d/%m/%Y')
+            datumFix.iloc[row,1] = datumFix.index[row].strftime('%H:%M:%S')
+    
         i=1
-        for key in frejmovi.keys():
-            dfCurrent=frejmovi[key]
-            colNames=dfCurrent.columns.values
-            colNames[0]=key
-            colNames[1]=colNames[1]+str(i)
-            colNames[2]=colNames[2]+str(i)
-            dfCurrent.columns=colNames
-            i=i+1
-            
-            #join/merge/concat to dfAll dataframe
-            dfAll=pd.merge(dfAll,dfCurrent,
-                           how='inner',
-                           left_index=True,
-                           right_index=True)
+        for key in keys:
+            if (key != u'Flag') and (key != u'Zone'):
+                dfTemp = frejmovi[key]
+                dfTemp.columns = [key, u'status.{0}'.format(i), u'flag.{0}'.format(i)]
+                i += 1
+                datumFix = datumFix.join(dfTemp, how='outer')
 
-        #write out csv file
-        dfAll.to_csv(filename)
+        #dodaj kljuceve 'Flag', 'Zone'
+        datumFix = datumFix.join(frejmovi[u'Flag'], how='outer')
+        datumFix = datumFix.join(frejmovi[u'Zone'], how='outer')
+
+        #napisi csv file, bez indeksa
+        datumFix.to_csv(filename,
+                        na_rep = '-999.00',
+                        sep = ',',
+                        encoding = 'latin-1',
+                        index = False)
+
     
     def load_work(self,filename):
-        """reads csv file, different from raw csv input"""
-        df=pd.read_csv(
-                    filename,
-                    na_values='-999.00',
-                    index_col=0,
-                    parse_dates=True,
-                    header=0,
-                    sep=',',
-                    encoding='latin-1'
-                    )
-        
-        #sastavljanje frejmova
-        reFlag='status'
-        reStatus='flag'
-        cols=list(df.columns)
-        frejmovi={}
-        keyList=[]
-        for col in cols:
-            match1=re.search(reFlag,col,re.IGNORECASE)
-            match2=re.search(reStatus,col,re.IGNORECASE)
-            #col ne smije matchati niti 'flag' niti 'status'            
-            if (not(match1 or match2)):
-                #col je key
-                keyList.append(col)
-                i=df.columns.get_loc(col)
-                frejmovi[col] = df.iloc[:,i:i+3]
-                tmp=frejmovi[col].columns.values
-                tmp[0]=u'koncentracija'
-                tmp[1]=u'status'
-                tmp[2]=u'flag'
-                frejmovi[col].columns=tmp
+        """
+        cita csv, nakon popravka citaca, ne treba drugu metodu
+        """
+        frejmovi = self.citaj(filename)
         return frejmovi
+#        df=pd.read_csv(
+#                    filename,
+#                    na_values='-999.00',
+#                    index_col=0,
+#                    parse_dates=True,
+#                    header=0,
+#                    sep=',',
+#                    encoding='latin-1'
+#                    )
+#        
+#        #sastavljanje frejmova
+#        reFlag='status'
+#        reStatus='flag'
+#        cols=list(df.columns)
+#        frejmovi={}
+#        keyList=[]
+#        for col in cols:
+#            match1=re.search(reFlag,col,re.IGNORECASE)
+#            match2=re.search(reStatus,col,re.IGNORECASE)
+#            #col ne smije matchati niti 'flag' niti 'status'            
+#            if (not(match1 or match2)):
+#                #col je key
+#                keyList.append(col)
+#                i=df.columns.get_loc(col)
+#                frejmovi[col] = df.iloc[:,i:i+3]
+#                tmp=frejmovi[col].columns.values
+#                tmp[0]=u'koncentracija'
+#                tmp[1]=u'status'
+#                tmp[2]=u'flag'
+#                frejmovi[col].columns=tmp
+#        return frejmovi
 
     def citaj_listu(self,lista):
         """
@@ -154,7 +209,6 @@ class WlReader:
         dataframe.
         -koristi funkciju citaj za citanje jednog elementa
         -spaja datafrejmove po indeksu i svim stupcima
-        -FULL OUTER JOIN
         """
         izlaz={}
         for element in lista:
@@ -163,20 +217,26 @@ class WlReader:
                 if kljuc not in izlaz:
                     izlaz[kljuc]=frejmovi[kljuc]
                 else:
-                    izlaz[kljuc]=pd.merge(
-                        izlaz[kljuc],
-                        frejmovi[kljuc],
-                        left_index=True,
-                        right_index=True,
-                        how='outer',
-                        on=[u'koncentracija',u'status',u'flag'],
-                        sort=True)
-
+                    izlaz[kljuc]=izlaz[kljuc].combine_first[frejmovi[kljuc]]
+#                    izlaz[kljuc]=pd.merge(
+#                        izlaz[kljuc],
+#                        frejmovi[kljuc],
+#                        left_index=True,
+#                        right_index=True,
+#                        how='outer',
+#                        on=[u'koncentracija',u'status',u'flag'],
+#                        sort=True)
         return izlaz
      
             
 if __name__ == "__main__":
-    data = WlReader().citaj('pj.csv')
+    sanity=WlReader()
+    data = sanity.citaj('pj.csv')
+    sanity.save_work(data,'test_save_work.csv')
+    data1 = sanity.load_work('test_save_work.csv')
+    test_frames(data,data1)
+    
+    #data = WlReader().citaj('pj.csv')   
     #save to file
     #WlReader().save_work(dataframe,filename as string)
     #load from csv

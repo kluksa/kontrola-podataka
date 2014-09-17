da676f49ed1c3de13a5e4187723e124be198a9f1
vmilic <milic.velimir@gmail.com>
2014-09-17 11:43:13 +0200

    Nastavak rada na dokumentu

--- dokument2.py
+++ dokument2.py
@@ -18,12 +18,45 @@ import os
 import citac
 import agregator
 
+###############################################################################
+###############################################################################
+"""
+PREBACI U DRUGI MODUL!
+
+Primjer implementacije citaca. Ovisno o izvoru podataka GUI instancira citac i
+postavlja ga u dokument.
+
+Jedine metode koje zanimaju dokument, i koje moraju postojati su:
+
+self.citaj(stanica, datum)
+    --> vraca sve frejmove za stanicu i datum koje uspije ucitati
+    --> frejmovi su dict {'kanal':dataframe sa podatcima}
+    Cilj ove metode je ucitati podatke iz izvora na zahtjev dokumenta.
+
+self.vrati_dostupne_fileove()
+    --> vraca dict svih kombinacija stanica/datum koje se mogu ucitati
+    Cilj ove metode je reci dokumentu sto se moze ucitati, Tada dokument moze
+    dati smislene zahtjeve za citanjem i moze updateati GUI (populiranje izbornika...)
+    
+Po slicnom principu ce se raditi drugi tipovi citaca. Prilikom instanciranja
+citacu se zada izvor podataka. Svaki konkretni citac ima svoj nacin kako
+ce implementirati metode citaj(stanica,datum) i vrati_dostupne_fileove(), ali API
+mora biti konzistentan.
+"""
 class TMReader(object):
     """
     ideja za weblogger citac.. instancira se zajedno sa izvorom podataka, 
     tj. folderom.U objekt su dodane metode za pronalazak dostupnih podataka.
     """    
     def __init__(self, folderPath):
+        """inicijalizacija citaca ide zajedno sa inicijalizaciojm foldera.
+        
+        FolderPath treba biti tipa string i treba biti put do foldera u kojem
+        se nalaze csv fileovi.
+        """
+        #TODO!
+        #import potrebnih modula prilikom inicijalizacije???
+        
         #import citac
         #import os
         #import re
@@ -36,12 +69,16 @@ class TMReader(object):
         self.files = []
         self.files_C = []
         
-        #pronalazak i spremanje svih dostupnih fileova
+        #pronalazak i spremanje svih dostupnih fileova prilikom inicijalizacije
+        #metoda populira dict self.dostupniFrejmovi iz kojeg se cita sa metodom
+        #self.citaj(stanica, datum)
         self.get_files()
         
     def citaj(self, stanica, datum):
         """Procitaj sve fileove pod kljucevima stanica, datum iz dostupnih fileova
         te vrati ucitane frejmove.
+        
+        Ulazni podatci su stringovi.
         
         BITNA METODA ZA DOKUMENT!
         Dokument poziva ovu metodu kada pokusava ucitati podatke.
@@ -49,12 +86,35 @@ class TMReader(object):
         za sve kanale. frejmovi = {'kanal':pd.DataFrame}
         """
         #dohvati listu fileova
+        #TODO!
+        #provjera ulaznih parametara? Potencijalni key error
+        #dokument mora znati koji su dostupni fileovi da bi znao updateati gui,
+        #pa se taj dio odgovornosti moze prebaciti na dokument
         files = self.dostupniFileovi[stanica][datum]
         #na svaki element liste treba nadodati folder path
         for index in list(range(len(files))):
             files[index]=os.path.join(self.folder, files[index])
-        
+        #iskoristi metodu specificnog citaca da procitas sve u listi i vratis frejmove
         return self.wlReader.citaj_listu(files)
+        
+    def vrati_dostupne_fileove(self):
+        """vrati dict, za svaku stanicu, koji datumi postoje u folderu
+        
+        BITNA METODA ZA DOKUMENT!
+        Ova metoda sluzi da dokumentu da informaciju do kojih sve fileova 
+        citac moze doci. Bitno za update kontrole GUI-a.
+        
+        Izlaz je dict {stanica:[lista datuma]} -->bitno jer cemo na slican nacin
+        pratiti sve fileove koji su ucitani i pohranjeni u dokumentu
+        """
+        izlaz ={}
+        #dohvati sve stanice do kojih mozes doci
+        stanice=list(self.dostupniFileovi.keys())
+        #za svaku piojedinacnu stanicu, napravi listu svih datuma
+        for stanica in stanice:
+            izlaz[stanica]=list(self.dostupniFileovi[stanica].keys())
+        #vrati rezultat
+        return izlaz
     
     #pomocne metode, samo priprema dostupnih podataka za citac
     def get_files(self):
@@ -114,28 +174,147 @@ class TMReader(object):
         datum = item[loc+1:loc+9]
         return stanica, datum
         
+###############################################################################
+###############################################################################
+"""
+Definicija posebne klase koja sluzi za spremanje podataka u dukumentu.
+Plan je da svaka stanica ima svoj objekt koji ce sadrzavati sve do tada ucitane
+frejmove. Razlog izdvajanja u posebni objekt je radi lakseg spremanja rada.
+
+npr. a=DataStorage('Bilogora')
+
+Cilj je definirati membere i metode da se na jednom mjestu sredi konzistentno
+updateanje frejmova prilikom ucitavanja novih podataka i dohvacanje vec ucitanih
+"""    
+class DataStorage(object):
+    """
+    Klasa za spremanje podataka sa JEDNE stanice.
+    SPECIFICNU INSTANCU GENERIRA DOKUMENT!
+    -kod instanciranja navedi ime postaje kao jedini argument tipa 'string'
+    """
+    def __init__(self, postaja):
+        """Konstruktor
         
-#    def citaj(self, stanica, datum):
-#        """
-#        PRVA BITNA metoda koju zanima dokument
-#        
-#        Mora procitati SVE dostupne podatke za kombinaciju stanica/datum.
-#        Mora sklopiti frejmove dict{kanal:pandas dataframe}
-#        Izlaz su frejmovi
-#        """
-#        pass
-#    
-#    def vrati_dostupne(self):
-#        """
-#        DRUGA BITNA metoda koju zanima dokument
-#        
-#        Mora vratiti sve dostupne stanice koje moze procitati
-#        Mora vratiti sve dostupne datume koje moze dohvatiti za stanicu
-#        
-#        izlaz je dict {stanica:[datum1, datum2, ...]}
-#        """
-#        pass
+        self.nazivPostaje
+            -definira kojoj stanici podatci pripadaju
+        
+        self.ucitaniFrejmovi
+            -dictionary frejmova na koje se dodaju ucitani podatci
+        
+        self.ucitaniDatumi
+            -lista koja prati koji su dani uneseni u ukupne frejmove
+        """
+        #inicijalizacija naziva postaje
+        self.nazivPostaje = postaja
+        #inicijalizacija praznog dicta gdje trebaju doci frejmovi
+        self.ucitaniFrejmovi = {}
+        #inicijalizacija ucitanih datuma (dana)
+        self.ucitaniDatumi = []
+        #inicijalizacija raspolozivih kanala
+        self.kanali = []
+                
+    def dodaj_frejm(self, datum, frejmovi):
+        """Dodaje frejmove na self.ucitaniFrejmovi
+        
+        ulaz:
+        datum - string
+        frejmovi - dict pandas datafrejmova {kanal:frejm minutnih podataka}
+        
+        BITNA METODA!
+        Uz pomoc ove metode, dokument sprema ucitane podatke na jedinstveni
+        dataframe.
+        
+        Metoda, za svaki kanal u frejmovi, radi update self.ucitaniFrejmovi.
+        Nadodaje sve vrijednosti indeksa koji nedostaju, te zatim preslikava
+        sve vrijednosti koje nisu NaN.
+        
+        Nakon updatea frejmova, appenda se datum na listu self.ucitaniDatumi.
+        Ta lista je bitna jer preko nje Dokument moze pratiti ucitane dane
+        """
+        #za sve kanale u ulaznim frejmovima
+        for kanal in frejmovi:
+            #provjeri da li kanal postoji u self.ucitaniFrejmovi
+            if kanal in self.ucitaniFrejmovi.keys():
+                #ako kanal vec postoji, "pametno" spoji frejmove
+                #1. dodaj sve indekse i vrijednosti koje nedostaju
+                #ako ima preklapanja indeksa, ostaju stare vrijednosti
+                self.ucitaniFrejmovi[kanal] = pd.merge(
+                    self.ucitaniFrejmovi[kanal], 
+                    frejmovi[kanal], 
+                    how = 'outer', 
+                    left_index = True, 
+                    right_index = True, 
+                    sort = True, 
+                    on = [u'koncentracija', u'status', u'flag'])
+                #2. updateaj frame sa svim vrijednostima koje nisu NaN
+                #ova metoda osigurava ako ima preklapanja indeksa, da se
+                #spreme novo ucitane vrijednosti
+                self.ucitaniFrejmovi[kanal].update(frejmovi[kanal])
+            else:
+                #slucaj kada dodajemo novi kanal
+                self.ucitaniFrejmovi[kanal] = frejmovi[kanal]
+                #dodaj novi kanal u popis raspolozivih kanala
+                self.kanali.append[kanal]
+        
+        #updateaj listu ucitanih datuma
+        
+        self.ucitaniDatumi.append(datum)
     
+    def dohvati_slice(self, kanal, granice):
+        """Metoda vraca slice frejmova unutar granica za odredjeni kanal
+        
+        BITNA METODA!
+        Preko nje, dokument dohvaca dijelove slicea koje priprema za crtanje i
+        obradu.
+
+        ulaz:
+        kanal - string, kljuc pod kojim se nalazi ciljani frejm
+        granice - lista [tmin, tmax] koja definira rubove slicea
+        
+        NAPOMENA:
+        -kanal mora biti valjan... inace, key error (odgovornost je na dokumentu
+        da preda validan zahtjev, jer moze provjeriti da li je kanal OK)
+        -tmin i tmax moraju biti pandas datetime timestampovi (tako su indeksi
+        pohranjeni u datafrejmu)
+        -tmin < tmax, inace izlaz je prazan dataframe
+        
+        izlaz: 
+        dataframe minutnih podataka unutar zadanih parametara
+        
+        P.S. izbjegavam direktni slice sa df.loc[tmin:tmax] jer ako ne postoje
+        indeksi tmin i tmax (situacija kada NEMA podataka), javlja se key error.
+        Ovo je workaround koji radi
+        """
+        #kopiraj dataframe
+        df = self.ucitaniFrejmovi[kanal].copy()
+        #napravi slice svih indeksa vecih ili jednakih donjoj granici
+        df = df[df.index >= granice[0]]
+        #od ostatka frejma, napravi slice svih indeksa manjih ili jednakih gornjoj
+        #granici
+        df = df[df.index <= granice[1]]
+        #vrati slice frejma        
+        return df
+    
+    def update_frejm(self, kanal, frejm):
+        """Update minutnih podataka samo jednog kanala
+        
+        BITNA METODA!
+        Metoda sluzi dokumentu da updatea promjene flagova (bilo da ih inicira
+        korisnik ili drugi dio programa). korisiti samo frejm kojeg si prije
+        dohvatio metodom self.dohvati_slice()
+        
+        ulaz:
+        kanal - string, kljuc pod kojim se nalazi ciljani frejm kojeg updateamo
+        frejm - dataframe sa novim podatcima koje trebamo spremiti, u biti to je
+        slajs frejma kojeg smo izvukli metodom self.dohvati_slice()
+        
+        izlaz:
+        update self.ucitaniFrejmovi[kanal]
+        """
+        #direktni update, jer baratamo direktnim sliceom kojeg smo dohvatili
+        self.ucitaniFrejmovi[kanal].update(frejm)
+###############################################################################
+###############################################################################
     
 
 class Dokument(QtGui.QWidget):

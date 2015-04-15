a7daf41eda41400d99bbc50836741f89d8837196
Luksa Kraljevic <kraljevic@cirus.dhz.hr>
2015-04-15 12:17:07 +0200

    Promijenjen nacin ucitavanja data framea

--- app/model/data_reader.py
+++ app/model/data_reader.py
@@ -73,34 +73,41 @@ class RESTReader(QtCore.QObject):
 
         try:
             #parse json i provjeri da li su svi relevantni stupci na broju
-            frame = pd.read_json(x, orient='records', convert_dates=['vrijeme'])
+            df = pd.read_json(x, orient='records', convert_dates=['vrijeme'])
             assert 'vrijeme' in frame.columns, 'ERROR - Nedostaje stupac: "vrijeme"'
             assert 'id' in frame.columns, 'ERROR - Nedostaje stupac: "id"'
             assert 'vrijednost' in frame.columns, 'ERROR - Nedostaje stupac: "vrijednost'
             assert 'statusString' in frame.columns, 'ERROR - Nedostaje stupac: "statusString"'
             assert 'valjan' in frame.columns, 'ERROR - Nedostaje stupac: "valjan"'
-            #zamjeni index u pandas timestamp (prebaci stupac vrijeme u index)
-            noviIndex = frame['vrijeme']
-            frame.index = noviIndex
-            #sacuvaj originalni id podatka (pod kojim je spremljen u bazu)
-            podatakId = frame['id']
-            #dohvati koncentraciju, zamjeni niske koncentracije sa np.nan
-            koncentracija = frame['vrijednost'].astype(np.float64)
-            koncentracija = koncentracija.map(self.nan_conversion)
-            #dohvati status i adaptiraj ga (sacuvaj i originalnu kopiju)
-            statusString = frame['statusString']
-            status = 0
-            #adapter za boolean vrijesnost valjan  (buduci flag)
-            valjan = frame['valjan']
-            valjan = valjan.map(self.valjan_conversion)
-            valjan = valjan.astype(np.int64)
 
-            #sklopi izlazni dataframe da odgovara API-u dokumenta
-            df = pd.DataFrame({'koncentracija':koncentracija,
-                               'status':status,
-                               'flag':valjan,
-                               'id':podatakId,
-                               'statusString':statusString})
+            df = df.set_index(df['vrijeme'])
+            df['status'] = pd.Series(0, df.index)
+            df.rename(columns={'vrijednost' : 'koncentracija'})
+            df['vrijednost'] = df['vrijednost'].map(self.nan_conversion)
+            df['valjan'] = df['valjan'].map(self.valjan_conversion)
+
+            # #zamjeni index u pandas timestamp (prebaci stupac vrijeme u index)
+            # noviIndex = frame['vrijeme']
+            # frame.index = noviIndex
+            # #sacuvaj originalni id podatka (pod kojim je spremljen u bazu)
+            # podatakId = frame['id']
+            # #dohvati koncentraciju, zamjeni niske koncentracije sa np.nan
+            # koncentracija = frame['vrijednost'].astype(np.float64)
+            # koncentracija = koncentracija.map(self.nan_conversion)
+            # #dohvati status i adaptiraj ga (sacuvaj i originalnu kopiju)
+            # statusString = frame['statusString']
+            # status = 0
+            # #adapter za boolean vrijesnost valjan  (buduci flag)
+            # valjan = frame['valjan']
+            # valjan = valjan.map(self.valjan_conversion)
+            # valjan = valjan.astype(np.int64)
+            #
+            # #sklopi izlazni dataframe da odgovara API-u dokumenta
+            # df = pd.DataFrame({'koncentracija':koncentracija,
+            #                    'status':status,
+            #                    'flag':valjan,
+            #                    'id':podatakId,
+            #                    'statusString':statusString})
 
         except (ValueError, AssertionError):
             #javi error signalom kontroleru da nesto nije u redu?
